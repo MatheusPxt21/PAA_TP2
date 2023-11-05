f53200d1e10af9e01371358a67ebd4c9ac2c4da4
Matheus Nogueira <95196338+MatheusNogueiraUfv@users.noreply.github.com>
2023-11-05 19:29:59 -0300

    Esqueleto da função recursiva Deslocar

--- Headers/Aventureiro.h
+++ Headers/Aventureiro.h
@@ -8,8 +8,7 @@ typedef struct Aventureiro{
 
 void InicializarAventureiro(MatrizMapa *ptrMapa,Aventureiro *ptrAv);
 void IniciarDeslocamento(MatrizMapa *ptr,Aventureiro *ptrAv,PilhaCoordenadas *PtrCoordenadas,PilhaRamificado *PtrRamificado,FilaPilhas *Filas);
-int Deslocar(MatrizMapa *ptr,Aventureiro *ptrAv,PilhaCoordenadas *PtrCoordenadas,PilhaRamificado *PtrRamificado,FilaPilhas *Filas);
-//int ExisteRamificacao(int i,int j,Aventureiro *ptrAv);
+int Deslocar(MatrizMapa *ptr, Aventureiro *ptrAv, int *i, int *j, int *LinhaRamificacao, int *ColunaRamificacao, PilhaCoordenadas *PtrCoordenadas, PilhaRamificado *PtrRamificado, FilaPilhas *Filas);
 int EsquerdaLivre(int j);
 int CimaLivre(int i);
 void RestaurarMatriz(int *i,int *j,PilhaCoordenadas *PtrCoordenadas,PilhaRamificado *PtrRamificado);
--- src/Aventureiro.c
+++ src/Aventureiro.c
@@ -24,84 +24,81 @@ void InicializarAventureiro(MatrizMapa *ptrMapa,Aventureiro *ptrAv){
 
 void IniciarDeslocamento(MatrizMapa *ptr,Aventureiro *ptrAv,PilhaCoordenadas *PtrCoordenadas,PilhaRamificado *PtrRamificado,FilaPilhas *Filas){
 
+    int i,j,LinhaRamificacao,ColunaRamificacao;
+    i = ptr->LinhasInicial;
+    j = ptr->ColunasInicial;
     InicializarAventureiro(ptr,ptrAv);
     initialize(PtrCoordenadas);
     inicializarRamificacao(PtrRamificado);
     initializeFilaPilhas(Filas);
-    Deslocar(ptr,ptrAv,PtrCoordenadas,PtrRamificado,Filas);
+    Deslocar(ptr,ptrAv,&i,&j,&LinhaRamificacao,&ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
 
 }
 
-int Deslocar(MatrizMapa *ptr,Aventureiro *ptrAv,PilhaCoordenadas *PtrCoordenadas,PilhaRamificado *PtrRamificado,FilaPilhas *Filas){
+int Deslocar(MatrizMapa *ptr, Aventureiro *ptrAv, int *i, int *j, int *LinhaRamificacao, int *ColunaRamificacao, PilhaCoordenadas *PtrCoordenadas, PilhaRamificado *PtrRamificado, FilaPilhas *Filas){
     //Ideia geral:
     // Semelhante ao TP anterior envolvendo o backtracking, a função irá explorar as ramificações, nesse caso esquerda e cima
     //A tabela será acrescida com os valores dos pontos de vida do jogador ao longo do trajeto pelo mapa
     //Quando uma ramificação for totalmente testada, o mapa é restaurado até aquela posição.
-    int i = ptr->LinhasInicial;
-    int j = ptr->ColunasInicial;
-    int control = 0;
-    int Esquerda = 0,Cima = 0;
-    //Ideia inicial fazer sem recursão
-    do{
-        if(ptrAv->PontosVidaAtual==0){
-            //Caso em que o jogador tenha os pontos de vida zerados
-        }
-        if(i==ptr->LinhasFinal && j==ptr->ColunasFinal && ptrAv->PontosVidaAtual>0){
-            //Foi encontrado um caminho válido até a saída, armazenar na fila de pilhas.
-            push(PtrCoordenadas,i,j,ptrAv->PontosVidaAtual);
-            InserirFilaPilhas(Filas,PtrCoordenadas,ptrAv->PontosVidaAtual);
 
-        }
-        //Nesse contexto com o mapa, não há obstáculos, os únicos pontos que não haverá ramificação será quando o jogador estiver na
-        // linha 0(impossível subir) e na coluna 0(impossível andar para esquerda
-        if(i>0 && j>0){
-            pushRamificacao(PtrRamificado,i,j,ptrAv->PontosVidaAtual);
-            push(PtrCoordenadas,i,j,ptrAv->PontosVidaAtual);
-            if(EsquerdaLivre(j) && PtrRamificado->topo->Esquerda==-1){
-                PtrRamificado->topo->Esquerda = 1;
-                j = j + 1;
-                control = 1;
-            }
-            if(control == 0 && CimaLivre(j) && PtrRamificado->topo->Cima==-1){
-                PtrRamificado->topo->Cima = 1;
-                i = i + 1;
-            }
+    if(ptrAv->PontosVidaAtual>0){
+        //Colocar a função de restaurar vida
+    }
+    //Caso a saida for encontrada e o jogador ainda estiver com vida
+    if(ptrAv->PontosVidaAtual>0 && *i == ptr->LinhasFinal && *j==ptr->ColunasFinal){
+        push(PtrCoordenadas,*i,*j,ptrAv->PontosVidaAtual);
+        InserirFilaPilhas(Filas,PtrCoordenadas,ptrAv->PontosVidaAtual);
+    }else if(ptrAv->PontosVidaAtual<=0){
 
-            if(PtrRamificado->topo->Cima!=-1 && PtrRamificado->topo->Esquerda!=-1){
-                popRamificacao(PtrRamificado);
-                //Adicionar Limpar Matriz
-            }
+    }
 
-        }else if(EsquerdaLivre(j) || CimaLivre(i)){
-            //Caso em que não há ramificação apenas esquerda ou cima são direções válidas
-            push(PtrCoordenadas,i,j,ptrAv->PontosVidaAtual);
-            if(EsquerdaLivre(j) && CimaLivre(i)!=1){
-                Cima = 0;
-                Esquerda = 1;
-            }else if(EsquerdaLivre(j)!=1 && CimaLivre(i)){
-                Esquerda = 0;
-                Cima = 1;
-            }
-            if(Esquerda){
-                j = j + 1;
-            }else if(Cima){
-                i = i + 1;
-            }
-            Esquerda = 0;
-            Cima = 0;
+    if(EsquerdaLivre(*j) && CimaLivre(*i)){
+        *LinhaRamificacao = *i;
+        *ColunaRamificacao = *j;
+        push(PtrCoordenadas,*i,*j,ptrAv->PontosVidaAtual);
+        pushRamificacao(PtrRamificado,*i,*j,ptrAv->PontosVidaAtual);
+        if(EsquerdaLivre(*j) && PtrRamificado->topo->Esquerda==-1){
+            PtrRamificado->topo->Esquerda = 1;
+            (*j) = (*j) + 1;
+            ptrAv->TabelaPD[*i][*j] = ptrAv->PontosVidaAtual + ptr->ConteudoMapa[*i][*j];
+            Deslocar(ptr,ptrAv,i,j,LinhaRamificacao,ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
+
+            //Função LimparMatriz e restaurar pontos de vida, voltar até o estado em que a ramificação foi feita.
+
         }else{
-            //Caso não tenha mais caminho.
+            PtrRamificado->topo->Esquerda = 0;
         }
-        ptrAv->PontosVidaAtual += ptr->ConteudoMapa[i][j];
-        ptrAv->TabelaPD[i][j] = ptrAv->PontosVidaAtual;
-        control = 0;
+        if(CimaLivre(*j) && PtrRamificado->topo->Cima==-1){
+            PtrRamificado->topo->Cima = 1;
+            (*i) = (*i) + 1;
+            ptrAv->TabelaPD[*i][*j] = ptrAv->PontosVidaAtual + ptr->ConteudoMapa[*i][*j];
+            Deslocar(ptr,ptrAv,i,j,LinhaRamificacao,ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
 
-        if(estaVazioRamificacao(PtrRamificado) && isEmptyFilaPilhas(Filas)!=1){
-            //Significa que um caminho foi encontrado e não tem mais ramificações a serem testadas.
-            return 1;
+            //Função LimparMatriz e restaurar pontos de vida, voltar até o estado em que a ramificação foi feita.
+
+        }else{
+            PtrRamificado->topo->Cima = 0;
         }
-    } while (estaVazioRamificacao(PtrRamificado)!=1);
-    //Nenhuma saída encontrada, imprimir impossível no arquivo de saída.
+
+        //Limpar a tabela já que a ramificação foi testada, retirar da pilha e restaurar os pontos de vida no ponto da última ramificação.
+        Deslocar(ptr,ptrAv,i,j,LinhaRamificacao,ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
+
+
+    }else{
+        //Apenas um caminho válido
+        if(EsquerdaLivre(*j)){
+            (*j) = (*j) + 1;
+            ptrAv->TabelaPD[*i][*j] = ptrAv->PontosVidaAtual + ptr->ConteudoMapa[*i][*j];
+            Deslocar(ptr,ptrAv,i,j,LinhaRamificacao,ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
+        }
+        if(CimaLivre(*i)){
+            (*i) = (*i) + 1;
+            ptrAv->TabelaPD[*i][*j] = ptrAv->PontosVidaAtual + ptr->ConteudoMapa[*i][*j];
+            Deslocar(ptr,ptrAv,i,j,LinhaRamificacao,ColunaRamificacao,PtrCoordenadas,PtrRamificado,Filas);
+
+        }
+    }
+
     return 0;
 }
 
